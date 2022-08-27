dbeee7076a5a1737600307365c7e09809cc1e6f2
João Manaia <joaopfmanaia@gmail.com>
2022-08-27 20:02:11 +0100

    first commit

---
+++ components/m3/header/header.tsx
@@ -0,0 +1,116 @@
+import { FC, useContext, useState } from "react"
+import {
+  AppBar,
+  Avatar,
+  Grid,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+  useScrollTrigger,
+  useTheme,
+} from "@mui/material"
+import MenuIcon from "@mui/icons-material/MenuTwoTone"
+import ColorIcon from "@mui/icons-material/ColorLensOutlined"
+import DarkIcon from "@mui/icons-material/DarkModeOutlined"
+import LightIcon from "@mui/icons-material/LightModeOutlined"
+import RestartIcon from "@mui/icons-material/RefreshOutlined"
+import { useRouter } from "next/router"
+import { ThemeModeContext } from "../../../app/theme/context/ThemeModeContext"
+import { ThemeSchemeContext } from "../../../app/theme/context/ThemeSchemeContext"
+import { ExitToAppRounded, PersonRounded } from "@mui/icons-material"
+
+interface HeaderProps {
+  onDrawerToggle?: () => void
+  window?: () => Window
+}
+
+interface HeaderAuthUserProps {
+  avatar?: string
+  name?: string
+  loggedIn: boolean
+}
+
+const Header: FC<HeaderProps> = ({ onDrawerToggle, window }) => {
+  const { palette } = useTheme()
+
+  const { toggleThemeMode, resetThemeMode } = useContext(ThemeModeContext)
+  const { generateThemeScheme, resetThemeScheme } = useContext(ThemeSchemeContext)
+
+  const changeThemeMode = () => toggleThemeMode()
+
+  const changeThemeScheme = async () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+    generateThemeScheme(randomColor)
+  }
+
+  const reset = () => {
+    resetThemeMode()
+    resetThemeScheme()
+  }
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  })
+
+  return (
+    <>
+      <AppBar color={trigger ? "primary" : "default"} position="sticky" elevation={trigger ? 2 : 0}>
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Grid item sx={{ display: { md: "none", sm: "block" } }}>
+              <Tooltip title="Menu">
+                <IconButton color="inherit" edge="start" onClick={onDrawerToggle}>
+                  <MenuIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+            <Grid item sx={{ display: "flex", alignItems: "baseline" }}>
+              <Typography
+                color="inherit"
+                sx={{ fontWeight: 500, letterSpacing: 0.5, fontSize: 20 }}
+              >
+                NewQuiz
+              </Typography>
+            </Grid>
+
+            <Grid item xs></Grid>
+
+            <Grid item>
+              <Tooltip title="Change Color">
+                <IconButton size="large" color="inherit" onClick={changeThemeScheme}>
+                  <ColorIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+            <Grid item>
+              <Tooltip title="Switch Theme">
+                <IconButton size="large" color="inherit" onClick={changeThemeMode}>
+                  {palette.mode == "light" ? <DarkIcon /> : <LightIcon />}
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+            <Grid item>
+              <Tooltip title="Reset">
+                <IconButton size="large" color="inherit" onClick={reset}>
+                  <RestartIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import ThemeModeProvider from "../app/theme/context/ThemeModeContext"
+import ThemeSchemeProvider from "../app/theme/context/ThemeSchemeContext"
+import M3ThemeProvider from "../app/theme/m3/M3ThemeProvider"
+import { CssBaseline } from "@mui/material"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeModeProvider>
+      <ThemeSchemeProvider>
+        <M3ThemeProvider>
+          <CssBaseline enableColorScheme />
+          <Component {...pageProps} />
+        </M3ThemeProvider>
+      </ThemeSchemeProvider>
+    </ThemeModeProvider>
+  )
 }
 
 export default MyApp
