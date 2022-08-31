290236b15ee318a31032cc45a11645a574a5108e
João Manaia <joaopfmanaia@gmail.com>
2022-08-31 10:56:09 +0100

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,24 @@ import ThemeModeProvider from "../app/theme/context/ThemeModeContext"
 import ThemeSchemeProvider from "../app/theme/context/ThemeSchemeContext"
 import M3ThemeProvider from "../app/theme/m3/M3ThemeProvider"
 import { CssBaseline } from "@mui/material"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeModeProvider>
-      <ThemeSchemeProvider>
-        <M3ThemeProvider>
-          <CssBaseline enableColorScheme />
-          <Component {...pageProps} />
-        </M3ThemeProvider>
-      </ThemeSchemeProvider>
-    </ThemeModeProvider>
+    <>
+      <Head>
+        <meta name="google-site-verification" content="vduNWIMxVDPQZoidzqpL-4nO41GAbOB_LOGrfAJAFms" />
+      </Head>
+
+      <ThemeModeProvider>
+        <ThemeSchemeProvider>
+          <M3ThemeProvider>
+            <CssBaseline enableColorScheme />
+            <Component {...pageProps} />
+          </M3ThemeProvider>
+        </ThemeSchemeProvider>
+      </ThemeModeProvider>
+    </>
   )
 }
 
