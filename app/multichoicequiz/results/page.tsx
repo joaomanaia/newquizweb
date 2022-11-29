a84a6959df2b04eddfd05bd5430f932de08e843f
joaomanaia <joaopfmanaia@gmail.com>
2022-11-29 19:38:52 +0000

    results screen nextjs fix

--- app/multichoicequiz/results/page.tsx
+++ app/multichoicequiz/results/page.tsx
@@ -1,27 +1,5 @@
-import { Typography } from "@mui/material"
-import QuizStepView from "../../../components/multichoicequiz/QuizStepView"
-import MultiChoiceQuestionStep, { Completed } from "../../../model/multichoicequiz/MultiChoiceQuestionStep"
-
 export default function Page() {
-  const questionSteps: MultiChoiceQuestionStep[] = []
-
   return (
-    <>
-      <Typography variant="h3">Congratulations</Typography>
-
-      <Typography variant="h5">2/5 Correct</Typography>
-
-      <div className="flex space-x-2">
-        {questionSteps.map((step, index) => (
-          <QuizStepView
-            key={step.question.id}
-            position={index + 1}
-            current={false}
-            correct={step instanceof Completed && step.correct}
-            completed={step instanceof Completed}
-          />
-        ))}
-      </div>
-    </>
+    <div>Results</div>
   )
 }
--- pages/api/multichoicequiz/randomQuestions.ts
+++ pages/api/multichoicequiz/randomQuestions.ts
@@ -18,28 +18,44 @@ const getOpenTDBQuestionsTest = async (amount: number = 5): Promise<OpenTDBQuest
     responde_code: 2,
     results: [
       {
-        category: "c",
-        question: "Who made NewQuiz?",
-        incorrect_answers: ["A", "B", "C"],
-        correct_answer: "D",
-        type: "t",
-        difficulty: "easy"
+        category: 'SGlzdG9yeQ==',
+        type: 'Ym9vbGVhbg==',
+        difficulty: 'bWVkaXVt',
+        question: 'VGhlIGZpcnN0IHRlbGV2aXNlZCBwcmVzaWRlbnRpYWwgZGViYXRlIHdhcyBiZXR3ZWVuIEppbW15IENhcnRlciBhbmQgR2VyYWxkIEZvcmQu',
+        correct_answer: 'RmFsc2U=',
+        incorrect_answers: [ 'VHJ1ZQ==' ]
       },
       {
-        category: "c",
-        question: "Hello?",
-        incorrect_answers: ["A", "B", "C"],
-        correct_answer: "D",
-        type: "t",
-        difficulty: "easy"
+        category: 'RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=',
+        type: 'bXVsdGlwbGU=',
+        difficulty: 'bWVkaXVt',
+        question: 'V2hhdCBpcyB0aGUgbmFtZSBvZiB0aGUgZmluYWwgYm9zcyBpbiBUdXJvazogRGlub3NhdXIgSHVudGVyPw==',
+        correct_answer: 'VGhlIENhbXBhaWduZXI=',
+        incorrect_answers: [ 'VGhlIFByaW1hZ2Vu', 'T2JsaXZpb24=', 'TG9yZCBUeXJhbm51cw==' ]
       },
       {
-        category: "c",
-        question: "Hello?",
-        incorrect_answers: ["A", "B", "C"],
-        correct_answer: "D",
-        type: "t",
-        difficulty: "easy"
+        category: 'RW50ZXJ0YWlubWVudDogTXVzaWM=',
+        type: 'Ym9vbGVhbg==',
+        difficulty: 'aGFyZA==',
+        question: 'UGV0ZSBUb3duc2hlbmQncyBzb2xvIGFsYnVtLCAiV2hpdGUgQ2l0eTogQSBOb3ZlbCIsIGlzIHNldCBpbiB0aGUgbWV0cm9wb2xpdGFuIGFyZWEgb2YgQ2hpY2Fnby4=',
+        correct_answer: 'RmFsc2U=',
+        incorrect_answers: [ 'VHJ1ZQ==' ]
+      },
+      {
+        category: 'RW50ZXJ0YWlubWVudDogQm9hcmQgR2FtZXM=',
+        type: 'bXVsdGlwbGU=',
+        difficulty: 'aGFyZA==',
+        question: 'SW4gTWFnaWM6IFRoZSBHYXRoZXJpbmcsIHdoYXQgdGVybSBmb3IgYmxvY2tpbmcgd2FzIGVzdGFibGlzaGVkIGluIHRoZSBQb3J0YWwgc2V0Pw==',
+        correct_answer: 'SW50ZXJjZXB0aW5n',
+        incorrect_answers: [ 'QmxvY2tpbmc=', 'UmVzaXN0aW5n', 'U2hpZWxkaW5n' ]
+      },
+      {
+        category: 'R2VvZ3JhcGh5',
+        type: 'bXVsdGlwbGU=',
+        difficulty: 'bWVkaXVt',
+        question: 'V2hhdCBjaXR5IGlzIHRoZSBUZXJyYWNvdHRhIEFybXkgbG9jYXRlZCBpbj8=',
+        correct_answer: 'WGknYW4=',
+        incorrect_answers: [ 'QmVpamluZw==', 'U2hhbmdoYWk=', 'SG9uZyBLb25n' ]
       }
     ]
   }
@@ -50,7 +66,7 @@ type Data = {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  getOpenTDBQuestions()
+  getOpenTDBQuestionsTest()
     .then((openTDBResponse) => openTDBResponse.results)
     .then((results) => results.map(convertOpenTDBResultToQuestion))
     .then((questions) => res.status(200).json({ questions: questions }))
