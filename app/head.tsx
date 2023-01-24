fe81fbb297c842a0376ca47e891a30617ed41846
João Manaia <joaopfmanaia@gmail.com>
2023-01-24 14:40:34 +0000

    added nextPWA

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,10 @@ export default function Head() {
     <>
       <title>NewQuiz</title>
       <meta name="description" content="The perfect game to increase your knowledge." />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name='viewport'
+        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+      />
       <link rel="icon" href="/favicon.ico" />
       <meta name="google-site-verification" content="vduNWIMxVDPQZoidzqpL-4nO41GAbOB_LOGrfAJAFms" />
     </>
