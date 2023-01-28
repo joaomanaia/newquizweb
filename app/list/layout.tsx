83051d47d9c348701926b5879ca41c146025417b
João Manaia <joaopfmanaia@gmail.com>
2023-01-28 19:50:49 +0000

    remove html tags in list layout

--- app/list/layout.tsx
+++ app/list/layout.tsx
@@ -1,12 +1,5 @@
 import MainLayout from "../components/m3/MainLayout"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-      <head />
-      <body>
-        <MainLayout>{children}</MainLayout>
-      </body>
-    </html>
-  )
+  return <MainLayout>{children}</MainLayout>
 }
