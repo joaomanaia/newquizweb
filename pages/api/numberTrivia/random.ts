abb5a2355a5619e2ac592492d77c3d25fbc80f30
joaomanaia <joaopfmanaia@gmail.com>
2023-02-18 22:39:30 +0000

    update random numberTrivia api to support multiple questions

--- pages/api/numberTrivia/random.ts
+++ pages/api/numberTrivia/random.ts
@@ -1,19 +1,64 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
-  number: number
-  question: string
+  questions: Question[]
+  message: string | undefined
 }
 
 const BASE_URL = "http://numbersapi.com/random/"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const min = req.query.min
-  const max = req.query.max
+  const min = reqQueryToNumber(req.query.min, 0)
+  const max = reqQueryToNumber(req.query.max, 100000)
+  const size = reqQueryToNumber(req.query.size, 1)
 
-  const url = (min && max) ? `${BASE_URL}?min=${min || ""}&max=${max || ""}&json=true` : `${BASE_URL}?json=true`
+  if (req.method !== "GET") {
+    res.status(400).end()
+    return
+  }
 
-  fetch(url)
+  if (size > 10) {
+    res.status(400)
+    res.json({ questions: [], message: "Max question size is 10, you requested: " + size })
+
+    return res.end()
+  }
+
+  res.setHeader("Content-Type", "application/json")
+
+  const promises: Promise<Question>[] = []
+
+  for (let i = 0; i < size; i++) {
+    promises.push(getRandomNumber(min, max))
+  }
+
+  const numbers = await Promise.all(promises)
+
+  res.status(200).json({ questions: numbers, message: undefined })
+  res.end()
+}
+
+const reqQueryToString = (data: string | string[] | undefined) => {
+  const dataString = Array.isArray(data) ? data[0] : data
+  return dataString ?? null
+}
+
+const reqQueryToNumber = (data: string | string[] | undefined, defaultNumber: number) => {
+  const dataString = reqQueryToString(data)
+
+  return parseInt(dataString ?? defaultNumber.toString())
+}
+
+type Question = {
+  number: number
+  question: string
+}
+
+const getRandomNumber = (min: number | null, max: number | null): Promise<Question> => {
+  const url =
+    min && max ? `${BASE_URL}?min=${min || ""}&max=${max || ""}&json=true` : `${BASE_URL}?json=true`
+
+  return fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const dataText: string = data.text
@@ -21,6 +66,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       // Remove the data number from the beginning of the string
       const question = dataText.substring(dataText.indexOf(" ") + 1)
 
-      res.status(200).json({ number: data.number, question: question })
-    }).catch((e) => res.status(404).end())
+      return { number: data.number, question: question }
+    })
 }
