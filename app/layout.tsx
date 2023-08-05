fc98949b8a33c0ebe103ee135071c3d71e4fc6f3
joaomanaia <joaopfmanaia@gmail.com>
2023-08-05 10:58:57 +0100

    added viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import "../styles/globals.css"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body id="__next">
         <M3>{children}</M3>
       </body>
