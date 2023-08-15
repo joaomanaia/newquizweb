e9376787b7e36929cb8ceec31ebd49624ff4b381
joaomanaia <joaopfmanaia@gmail.com>
2023-08-15 15:29:08 +0100

    improve metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-"use client"
 import M3 from "@/core/theme/M3"
 import "../styles/globals.css"
+
+export const metadata = {
+  manifest: "/manifest.json",
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+      <head></head>
       <body id="__next">
         <M3>{children}</M3>
       </body>
