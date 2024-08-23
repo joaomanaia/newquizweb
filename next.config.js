a08927aad6123d1d066b3498b4b17e06330f9059
joaomanaia <joaopfmanaia@gmail.com>
2024-08-23 19:25:25 +0100

    remove next-pwa and update dependencies versions

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    typedRoutes: true
+    typedRoutes: true,
   },
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
@@ -19,8 +13,8 @@ const nextConfig = {
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
