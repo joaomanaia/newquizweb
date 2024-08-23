8eaf459f57c69d689a710062986833ea9497a110
joaomanaia <joaopfmanaia@gmail.com>
2024-08-23 18:53:53 +0100

    Remove next-sitemap

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://newquiz-app.vercel.app/",
-  generateRobotsTxt: true,
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss"
+
+const config = {
   corePlugins: {
-    preflight: false
+    preflight: false,
   },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
-    './core/theme/components/**/*.{js,ts,jsx,tsx,mdx}'
+    "./core/theme/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   important: "#__next",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
+
+export default config
