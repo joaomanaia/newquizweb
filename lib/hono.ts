c0a486e544116f221b04d57af3822f1872aa8c41
joaomanaia <joaopfmanaia@gmail.com>
2026-02-19 21:25:11 +0000

    hono temp fix

--- lib/hono.ts
+++ lib/hono.ts
@@ -2,4 +2,4 @@ import { hc } from "hono/client"
 import type { AppType } from "@/app/api/[[...route]]/route"
 import { env } from "@/src/env"
 
-export const client = hc<AppType>(env.VERCEL_URL ?? env.NEXT_PUBLIC_APP_URL)
+export const client = hc<AppType>(/* env.VERCEL_URL ??  */ env.NEXT_PUBLIC_APP_URL)
