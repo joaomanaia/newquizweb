43993e9918a15dbb63bf20686bb969ff2461f277
joaomanaia <joaopfmanaia@gmail.com>
2026-02-21 14:42:25 +0000

    Remove unused code

--- src/app/(core)/list/multichoicequiz/_lib/searchParams.ts
+++
@@ -1,6 +0,0 @@
-import { createLoader, parseAsStringLiteral } from "nuqs/server"
-import { difficulties } from "@/types/question-difficulty"
-
-export const multichoiceQuizSearchParams = createLoader({
-  difficulty: parseAsStringLiteral(difficulties),
-})
--- src/core/util/Network.ts
+++
@@ -1,5 +0,0 @@
-export const isLocalHost = (): boolean => process.env.NODE_ENV === "development"
-
-export const getHost = (): string => {
-  return isLocalHost() ? "http://localhost:3000" : "https://newquiz-app.vercel.app"
-}
